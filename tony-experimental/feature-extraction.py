887fd367903e5680da45e0b8487e30b31de4d18e
Tony Lee <22460852+ssbmaster@users.noreply.github.com>
2020-06-19 01:10:09 -0400

    Read and display one sound file from each class Scipy.io

--- tony-experimental/feature-extraction.py
+++ tony-experimental/feature-extraction.py
@@ -8,33 +8,55 @@ import pandas as pd
 
 # %% Read the metadata
 metaData = pd.read_csv('../data/UrbanSound8K/metadata/UrbanSound8K.csv')
+classes = list(metaData['class'].unique()) 
 
-# %% Read audio input (1 file for now)
-sampleRate, soundData = read('../data/UrbanSound8K/audio/fold1/7061-6-0-0.wav')
-sDataMono = soundData[:,0]
-clipLength = soundData.shape[0] / sampleRate
-print(f'Sample rate = {sampleRate}')
-print(f'Data points = {soundData.shape[0]}')
-print(f'Number of channels = {soundData.shape[1]}')
-print(f'Length = {clipLength}s')
+# %% Load functions
 
-# %% Play Audio
-Audio(sDataMono, rate=sampleRate)
+# Function to plot the waveform (stereo)
+def plt_orig_waveform(sampleRate, soundData, channels):
+    if channels == 'mono':
+        soundData = soundData[:,0]
+    clipLength = soundData.shape[0] / sampleRate
+    time = np.linspace(0, clipLength, soundData.shape[0])
+    plt.plot(time, soundData[:, 0], label="Left channel")
+    plt.plot(time, soundData[:, 1], label="Right channel")
+    plt.legend()
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude")
+    plt.show()
+    print(f'Sample rate = {sampleRate}')
+    print(f'Data points = {soundData.shape[0]}')
+    print(f'Number of channels = {soundData.shape[1]}')
+    print(f'Length = {clipLength}s')
 
-# %% Plot the waveform (mono)
-plt.figure()
-plt.plot(sDataMono)
-plt.xlabel('Sample Index')
-plt.ylabel('Amplitude')
-plt.title('Waveform of Audio')
-plt.show
+# %% Grab one audio file from each class
+files = dict()
+for i in range(len(classes)):
+    tmp = metaData[metaData['class'] == classes[i]][:1].reset_index()
+    path = (
+            '../data/UrbanSound8K/audio/fold{}/{}'
+            .format(tmp['fold'][0], tmp['slice_file_name'][0])
+            )
+    files[classes[i]] = path
+
+# %% Plot the waveforms
+fig = plt.figure(figsize=(15,15))# Log graphic of waveforms to Comet
+fig.subplots_adjust(hspace=0.4, wspace=0.4)
+for i, label in enumerate(classes):
+    fn = files[label]
+    fig.add_subplot(5, 2, i+1)
+    plt.title(label)
+    sampleRate, soundData = read(fn) # alt: librosa.load(fn)
+    Audio(soundData[:,0], rate=sampleRate) # IPython play audio widget
+    plt_orig_waveform(sampleRate, soundData, 'stereo')
+    # alt: librosa.display.waveplot(data, sr= sample_rate)
+plt.savefig('class_examples.png')
 
 # %%
-# Plot the waveform (stereo)
-time = np.linspace(0, clipLength, soundData.shape[0])
-plt.plot(time, soundData[:, 0], label="Left channel")
-plt.plot(time, soundData[:, 1], label="Right channel")
-plt.legend()
-plt.xlabel("Time [s]")
-plt.ylabel("Amplitude")
-plt.show()
+for x in range(1250,1500):
+    print(soundData[x,0])
+
+# %%
+# https://towardsdatascience.com/how-to-apply-machine-learning-and-deep-learning-methods-to-audio-analysis-615e286fcbbc
+# https://haythamfayek.com/2016/04/21/speech-processing-for-machine-learning.html
+# http://practicalcryptography.com/miscellaneous/machine-learning/guide-mel-frequency-cepstral-coefficients-mfccs/
