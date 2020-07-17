48a1c3d16f1b3f184b121d7ddefe327a38273fbc
Tony Lee <22460852+ssbmaster@users.noreply.github.com>
2020-07-17 01:52:18 -0400

    updated data-exploration for newer plots

--- data-exploration_parallel.py
+++ data-exploration_parallel.py
@@ -29,7 +29,7 @@ file_data
 # ## Overview of distributions of the data
 # ### Plot the overall counts of the categories
 ax = sns.countplot(x='class', data=file_data)
-ax.set_title('Overall count of audio files by category')
+# ax.set_title('Overall count of audio files by category')
 ax.set_xticklabels(
     ax.get_xticklabels(), 
     rotation=45, 
@@ -43,23 +43,25 @@ plt.show()
 # ### Plot the counts of the categories by fold
 facets = sns.catplot(
     data=file_data,
-    col_wrap=5,
+    height=1.5,
+    aspect=2.5,
+    col_wrap=2,
     x='class',
     kind='count',
     col='fold'
 )
 facets.set_xticklabels(rotation=45, horizontalalignment='right')
 facets.set_titles('Fold {col_name}')
-plt.xlabel('Class')
-plt.ylabel('Count')
-plt.suptitle('Counts of categories by fold', y=1.05)
+plt.xlabel('class')
+plt.ylabel('count')
+# plt.suptitle('Counts of categories by fold', y=1.05)
 plt.show()
 
 
 # %% [markdown]
 # ### See how Audio Length/Duration is distributed
 
-# sample down
+# Sample down if needed
 # file_data = file_data.groupby('class', as_index=False).apply(lambda x: x.sample(10))
 
 file_data['raw_audio_tuple'] = Parallel(n_jobs=-1)(delayed(
@@ -76,21 +78,29 @@ file_data['duration'] = [
 
 # %% [markdown]
 # #### In particular, see the distribution of clip length vs class
-ax = sns.boxplot(x="class", y="duration", data=file_data)
+ax = sns.boxplot(x="class",
+                 y="duration", 
+                 data=file_data,
+                 showmeans=True,
+                 meanprops={"marker":"o",
+                            "markerfacecolor":"white", 
+                            "markeredgecolor":"black",
+                            "markersize":"7"})
 ax.set_xticklabels(
     ax.get_xticklabels(), 
     rotation=45, 
     horizontalalignment='right',
 )
-ax.set_title('Distribution of Clip Length vs Class')
+# ax.set_title('Distribution of Clip Length vs Class')
 plt.xlabel('Class')
 plt.ylabel('Duration (seconds)')
+
 plt.show()
 
 # %% [markdown]
 # #### Also, see the count of various clip lengths
 ax = sns.distplot(file_data['duration'], bins=20, kde=False)
-ax.set_title('Count of Clip Lengths')
+# ax.set_title('Count of Clip Lengths')
 plt.xlabel('Duration (seconds)')
 plt.ylabel('Count')
 plt.show()
@@ -99,27 +109,30 @@ plt.show()
 # ### Plot Waves and Spectrogram
 # #### Plot waves, one for each class
 from IPython.display import Audio
-plt.figure(figsize=(20,60))
+plt.figure(figsize=(6,9))
 plt.tight_layout()
 for i in range(len(CLASSES)):
     selection = file_data[file_data['class'] == CLASSES[i]][:1].reset_index()
     plt.subplot(10,1,i+1)
     librosa.display.waveplot(selection['raw_features'][0], 
                              sr=selection['sample_rate'][0])
-    plt.title(selection['class'][0])
-plt.suptitle('Wave Plot for each class', y=0.895, fontsize=16)
+    plt.title(selection['class'][0], fontsize=12, y=0.25)
+# plt.suptitle('Wave Plot for each class', y=0.895, fontsize=16)
 plt.show()
 # Audio(selection['raw_features'][0], rate=selection['sample_rate'][0])
 
 # %% [markdown]
 # ####  Plot spectrogram, one for each class
-plt.figure(figsize=(20,60))
+plt.figure(figsize=(6,9))
 plt.tight_layout()
 for i in range(len(CLASSES)):
     selection = file_data[file_data['class'] == CLASSES[i]][:1].reset_index()
     plt.subplot(10,1,i+1)
     plt.specgram(selection['raw_features'][0], 
-             Fs=selection['sample_rate'][0])
-    plt.title(selection['class'][0])
-plt.suptitle('Spectrogram for each class', y=0.895, fontsize=16)
+                 Fs=selection['sample_rate'][0]
+                )
+    plt.title(selection['class'][0], fontsize=12, y=0)
+# plt.suptitle('Spectrogram for each class', y=0.895, fontsize=16)
 plt.show()
+
+# %%
